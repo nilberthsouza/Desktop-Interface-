c3b7837399400496b086377d73ba1105c825b747
nilberthsouza <nilberth@protonmail.com>
2019-11-26 12:54:47 -0300

    list box adcionado , scrollbar adcionado, list box conectado a scrollbar

--- part_manager.py
+++ part_manager.py
@@ -32,10 +32,14 @@ price_entry = Entry(app, textvariable= price_text)
 price_entry.grid(row=1,column=3)
 
 #Parts List(ListBox)
-parts_list = Listbox(app, height=8, width=50)
+parts_list = Listbox(app, height=8, width=50,border=0)
 parts_list.grid(row=3, column=0, columnspan=3,rowspan=6, pady=20, padx=20)
-
-
+#create scroll
+scrollbar = Scrollbar(app)
+scrollbar.grid(row=3, column=3)
+#set scroll to listbox
+parts_list.configure(yscrollcommand=scrollbar.set)
+scrollbar.configure(command=parts_list.yview)
 
 app.title('Part Manager')
 app.geometry('700x350')
