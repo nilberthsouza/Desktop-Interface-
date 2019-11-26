91227b48e5112c1f3cdce2b669a26abe7878c1fa
nilberthsouza <nilberth@protonmail.com>
2019-11-26 13:47:33 -0300

    GUI finalizada, banco de dados montado, chamadas do banco de dados(db.py) finalizado, botão de add funcionado com tratamento de erros para campos vazios, list box ja popula campos automaticamente

--- part_manager.py
+++ part_manager.py
@@ -1,6 +1,42 @@
 from tkinter import *
+from db import Database
+from tkinter import messagebox
+db = Database('store.db')
+
+
+
+
+def populate_list():
+    #certifica que não há nada no listbox antes de mostrar o resultado do db
+    parts_list.delete(0,END)
+    for row in db.fetch():
+        parts_list.insert(END, row)
+
+def add_item():
+    if part_text.get() == '' or customer_text() == '' or retailer_text.get() == '' or price_text.get() == '':
+        messagebox.showerror('Required Field', 'Please include all field')
+        return 
+    db.insert(part_text.get(),customer_text.get(), retailer_text.get(), price_text.get())
+    parts_list.delete(0,END)
+    parts_list.insert(END,(part_text.get(),customer_text.get(),retailer_text.get(),price_text.get()))
+    populate_list()
+
+def remove_item():
+    print('remove')
+def update_item():
+    print('Update')
+
+def clear_text():
+    print('clear')
+
+
 
 app = Tk()
+
+
+
+
+
 
 #part
 part_text = StringVar()
@@ -41,8 +77,24 @@ scrollbar.grid(row=3, column=3)
 parts_list.configure(yscrollcommand=scrollbar.set)
 scrollbar.configure(command=parts_list.yview)
 
+#buttons
+add_btn = Button(app, text='Add Part',width=12,command=add_item)
+add_btn.grid(row=2, column=0,pady=20)
+
+remove_btn = Button(app, text='Remove',width=12,command=remove_item)
+remove_btn.grid(row=2, column=1)
+
+update_btn = Button(app, text='Update',width=12,command=update_item)
+update_btn.grid(row=2, column=2)
+
+clear_btn = Button(app, text='Clear Input',width=12,command=clear_text)
+clear_btn.grid(row=2, column=3)
+
+
+
 app.title('Part Manager')
 app.geometry('700x350')
 
+populate_list()
 
 app.mainloop()
