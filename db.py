b7a6ce874ac58b860da91219a8369560581063cf
nilberthsouza <58176211+nilberthsouza@users.noreply.github.com>
2020-01-15 08:31:10 -0300

    Update db.py

--- db.py
+++ db.py
@@ -4,7 +4,7 @@ class Database:
     def __init__(self, db):
         self.conn = sqlite3.connect(db)
         self.cur = self.conn.cursor()
-        self.cur.execute("CREATE TABLE IF NOT EXISTS parts (id INTENGER PRIMARY KEY, part text,customer text, retailer text, price text)")
+        self.cur.execute("CREATE TABLE IF NOT EXISTS parts (id INTEGER PRIMARY KEY, part text,customer text, retailer text, price text)")
         self.conn.commit()
     def fetch(self):
         self.cur.execute("select * FROM parts")
